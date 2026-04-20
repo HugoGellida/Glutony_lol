2ee50c39c9573a412aa4d61779f0844fe4e6298f
3nd3rh3o <theenderman3333@gmail.com>
2026-04-20 15:50:52 +0200

    sync

---
+++ Glutony/scr/built-in/sceneScript/PhysDemo.cpp
@@ -0,0 +1,27 @@
+#include <common/Scene.hpp>
+#include <iostream>
+
+void sceneMain(Scene& scene)
+{
+	dataStruct::Material * mat   = scene.resolveMaterialAsset("built-in/materials/lit_phong.mat");
+    component::Mesh * mesh = scene.resolveMeshAsset("built-in/mesh/cube_n.obj");
+    if (mat == nullptr || mesh == nullptr)
+        return;
+    
+    for (size_t i = 0; i < 10; ++i)
+    {
+        GameObject * cube = scene.addGameObject("Cube " + std::to_string(i));
+        cube->transform.setPosition(glm::vec3(i * 2.0f, 0.0f, 0.0f));
+        cube->transform.setRotation(glm::vec3(0.0f, i * 15.0f, 0.0f));
+        cube->transform.setScale(glm::vec3(0.1f));
+        cube->addComponent(new component::MeshRenderer(mesh, mat));
+        cube->addComponent(new physics::BoxCollider(glm::vec3(0.0f), glm::vec3(0.5f), glm::vec3(1.0f)));
+        cube->addComponent(new physics::RigidBody(cube));
+        physics::RigidBody* rigidBody = cube->getComponent<physics::RigidBody>();
+        rigidBody->mass = 1.0f;
+        rigidBody->useGravity = true;
+        rigidBody->linearDamping = 0.1f;
+        rigidBody->angularDamping = 0.05f;
+        rigidBody->bounciness = 0.3f;
+    }
+}
